f650fc7f1dffbbf2c37dacddfbf7aee4c457d1d3
mahaa <mahaa@MAHA>
2024-02-04 16:42:54 +0200

    all tests solved and updated

--- Root-classificatin/src/test/java/Root_classificatin/Root_classificatin/RootsTest.java
+++ Root-classificatin/src/test/java/Root_classificatin/Root_classificatin/RootsTest.java
@@ -7,16 +7,79 @@ public class RootsTest
 {
 	Roots root;
 
+	
+	// Test for a not quadratic equation  a=0
 	@Test
-	public void testPartion1() 
+	public void BvaMin() 
 	{
-		//(0,1,2)
-		Roots.calculate_roots(-2, 1, 2);
+		//(0,2,3)
+		Roots.calculate_roots(0,2,3);
 		
-		String  root_expected = Root_Types.ERROR.toString();
-		
-		
+		String  root_expected = Root_Types.Not_quadratic.toString();
 		assertEquals(root_expected, Roots.root_types().toString());
 	}
+	
+	
+	/// Test for  quadratic equation with two roots 
+	@Test 
+	public void BvaMinPlus1(){
+		
+		//(1,5,4)
+		Roots.calculate_roots(1,5,4);
+				
+		String  root_expected = Root_Types.Two_roots.toString();
+		assertEquals(root_expected, Roots.root_types().toString());
+		
+	}
+	
+	
+	/// Test for  nominal a
+		@Test 
+		public void BvaNominal(){
+			
+			//(10,10,2)
+			Roots.calculate_roots(10,10,2);
+					
+			String  root_expected = Root_Types.Two_roots.toString();
+			assertEquals(root_expected, Roots.root_types().toString());
+			
+		}
+		
+		
+	//Test for a max minus 1 with no roots 
+		@Test 
+		public void BvaMaxMinus1() {
+			
+			//(99,3,1)
+			Roots.calculate_roots(99,3,1);
+			
+			String  root_expected = Root_Types.No_roots.toString();
+			assertEquals(root_expected, Roots.root_types().toString());
+			
+		}
+		
+		
+		
+		
+	//Test for a max  with one root Equal roots 
+		@Test 
+		public void BvaMax() {
+			
+			//(100,40,4)
+			Roots.calculate_roots(100,40,4);
+			
+			String  root_expected = Root_Types.Equal_roots.toString();
+			assertEquals(root_expected, Roots.root_types().toString());
+			
+		}
+		
+		
+		
+		
+		
+		
+	
+	
+	
 	
 	}
